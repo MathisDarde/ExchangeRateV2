5e3e99e1a866040dc9f3563fc80d6d1801e19f40
Mathis Dardé <dardemathis@gmail.com>
2024-09-18 09:14:20 +0200

    ajout de la fonction converter qui va convertir la valeur entrée par l'utilisateur dans la monnaie souhaitée

--- converter.js
+++ converter.js
@@ -1,3 +1,20 @@
+async function converter() {
+  if (source.value != "0" && cible.value != "0" && number.value) {
+    const devise = await fetch(
+      `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${source.value}/${cible.value}/${number.value}`
+    );
+    let resultat_chiffre = await devise.json();
+    console.log(resultat_chiffre);
+    resultat.innerHTML = resultat_chiffre.conversion_result;
+  } else if (source.value == "0") {
+    resultat.innerHTML = "Choisir une devise source";
+  } else if (!number.value) {
+    resultat.innerHTML = "Entrer un montant";
+  } else {
+    resultat.innerHTML = "Choisir une devise cible";
+  }
+}
+
 async function LoadDevise() {
   const devise = await fetch(
     `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/EUR`
@@ -25,5 +42,9 @@ let resultat = document.getElementById("resultat");
 
 const source = document.getElementById("devise_source");
 const cible = document.getElementById("devise_cible");
+
+number.addEventListener("input", converter);
+source.addEventListener("input", converter);
+cible.addEventListener("input", converter);
 
 LoadDevise();
